55a62b05d05b215c1b9bdea3b4d24da6897e4e37
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2024-10-22 13:03:31 +0200

    klar

--- Diagnos 1/Program.cs
+++ Diagnos 1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hej och välkommen till gissningsleken, här så ska du gissa på ett ord som jag tänker på!");
+Console.WriteLine("Hur många gånger vill du gissa på ordet 1 - 3?");
+int skib = 3;
+if (skib == 4)
+{
+    Console.WriteLine("du måste välja mellan 1 och 3");
+    Console.ReadLine();
+}
+string ratt = "sås";
+if (ratt = true)
+{
+
+}
+
+Console.ReadLine();
+
+//jag kan inte lösa denna diagnos jag har glömt hur man gör 
